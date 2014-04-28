b18a67525be96ed82ddf661a525bc7cfdb92b141
to93m <tomo.arbc@gmail.com>
2014-04-28 14:00:11 +0900

    a little chage

--- warrange.rb
+++ warrange.rb
@@ -1,3 +1,5 @@
+#ウィジェットごとに配置可能座標の数を出力
+
 line = gets.split(' ')
 H = line[0].to_i
 W = line[1].to_i
@@ -8,3 +10,17 @@ H.times{|i|
   position[i] = line.split("")
 }
 
+N = gets.chomp.to_i
+wiget = Array.new(N).map!{Array.new(2, 0)}
+N.times{|i|
+  line = gets.split(' ')
+  wiget[i][0] = line[0]
+  wiget[i][1] = line[1]
+}
+
+dinate_total = H * M
+
+N.times{|i|
+  wiget_size = wiget[i][0] * wiget[i][1]
+  max_num = wiget_size
+}
